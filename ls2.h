588b35ffbbd1c95407d3f7911ad9e58d3e63d4eb
maddyS <hidngemini@gmail.com>
2025-02-14 02:31:40 +0000

    .h comments

--- ls2.h
+++ ls2.h
@@ -7,13 +7,10 @@
 #define FALSE 0
 #define INDENT "    "
 
-// TODO: Any global variables go below
-//int thisIsGlobal1 = 10;  // delete before submission
-
 void indent(int); // indents n times based on input
 void printFullDir(char*); // calls recursivePrintFullDir with depth 0
 void recursivePrintFullDir(char*, int); // recursively prints all files in directory
-void printMatchingFiles(char*, char*);
-int recursivePrintMatchingFiles(char*, int, char*, stack_t*);
+void printMatchingFiles(char*, char*); // creates a stack, calls recursivePrintFulLDir, and prints the stack
+int recursivePrintMatchingFiles(char*, int, char*, stack_t*); // recursively prints all matching files in directory
 
 #endif
